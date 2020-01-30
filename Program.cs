03532fe0cab35d43fded87720011dcf3fde0ff77
Sina Tahan <sina9tahan@gmail.com>
2020-01-30 14:22:59 +0330

    can Pack and UnPack

--- Program.cs
+++ Program.cs
@@ -6,42 +6,109 @@ namespace file_format
 {
     class Program
     {
-        static List<FileInfo> FileList= new List<FileInfo>();
-
+        static List<string> FileList= new List<string>();
 
         static void Main(string[] args)
         {
-            string OutputDir = "C:\\Users\\sina\\Desktop\\output";
-            string InputDir = "C:\\Users\\sina\\Desktop\\input";
-            string TextFileName = "TALLLOOOOOS.txt";
-            string TextInputPath = System.IO.Path.Combine(InputDir, TextFileName);
-            string TextOutputPath = System.IO.Path.Combine(OutputDir, TextFileName);
+        string outputDir = "C:\\Users\\sina\\Desktop\\output";
+        string InputDir = "C:\\Users\\sina\\Desktop\\input";
+        string File1Name = "TALLLOOOOOS.txt";
+        string File2Name = "face.jpg";
+        string OutputFileName = "File.nko";
+        string InputPath = System.IO.Path.Combine(InputDir, File1Name);
+        string InputPath2 = System.IO.Path.Combine(InputDir, File2Name);
+        string OutputPath = System.IO.Path.Combine(outputDir, OutputFileName);
 
-            byte[] buffer = new byte[256];
-            int b_size = buffer.Length;
-            FileStream InputStream = File.OpenRead(TextInputPath);
-
-            FileStream OutputStream = File.OpenWrite(TextOutputPath);
-            
-
-            //Console.WriteLine(test +" ---- "+ NumberOfChunks);
-            
-    
-            while(InputStream.Position != InputStream.Length)
+        FileList.Add(InputPath);
+        FileList.Add(InputPath2);
+        //DisplayValues(OutputPath);
+        //Pack(OutputPath, FileList);
+        UnPack(OutputPath, Path.Combine(InputDir,"answer"));
+        Console.WriteLine($"OutputPath = {OutputPath} \nInputDir = {Path.Combine(InputDir,"answer")}");
+        Console.ReadLine();
+        
+        }
+        private static void UnPack(string MergedFile, string OutputPath)
+        {
+            using(var MergedStream = File.OpenRead(MergedFile)) // first pool
+            using(BinaryReader reader = new BinaryReader(MergedStream)) // to read bytes from the merged file
             {
-                Console.Write("InputStream: "+""+InputStream.Position + "-");
-                InputStream.Read(buffer, 0, b_size);
-                OutputStream.Write(buffer, 0, b_size);
-                Console.WriteLine(InputStream.Position);
-                Console.WriteLine("OutPutStream: "+""+ OutputStream.Position);
-                
+                var FileCount = reader.ReadInt32();
+                for(int i = 0; i < FileCount; i++)
+                {
+                    long FileSize = reader.ReadInt64();
+                    string FileName = reader.ReadString();
+                    Console.WriteLine($"size: {FileSize}name: {FileName}");
+                    using var OutputStream = File.OpenWrite(Path.Combine(OutputPath,FileName));//second pool
+                    while(OutputStream.Position!= FileSize)
+                    {
+                        var BytesLeft = FileSize - OutputStream.Position;
+                        var BufferSize = Math.Min(BytesLeft, 1024*1024*2);
+                        var buffer = new byte[BufferSize];
+                        MergedStream.Read(buffer, 0, (int)BufferSize);
+                        OutputStream.Write(buffer, 0, (int)BufferSize);     
+                    }
+                    
+                }
+            }
+        }
+        private static void Copy(string inputFile,string outputFile)
+        {
+            using (var inputStream = File.OpenRead(inputFile))
+            using (var outputStream = File.OpenWrite(outputFile))
+            {
+                while (inputStream.Position != inputStream.Length)
+                {
+                    var buffer = new byte[1024*1024];
+                    var bytesRead = inputStream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead < buffer.Length)
+                    {
+                        var newBuffer = new byte[bytesRead];
+                        for (int i = 0; i < bytesRead; i++)
+                        {
+                            newBuffer[i] = buffer[i];
+                        }
+                        buffer = newBuffer;
+                    }
+                    
+                    outputStream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
 
-            }  
+        private static void Pack(string OutPut, List<string> FileList)
+        {
+            using (var outputStream = File.OpenWrite(OutPut))
+            {
+                using (BinaryWriter writer = new BinaryWriter(outputStream))
+                {
 
-            OutputStream.Dispose();
-            InputStream.Dispose();
-            Console.ReadLine();
+                writer.Write(FileList.Count);
+                foreach (var item in FileList)
+                {
+                    using (var inputStream = File.OpenRead(item))
+                    {
+                     writer.Write(inputStream.Length);
+                     writer.Write(Path.GetFileName(item));//is the path to the file
+                     while (inputStream.Position != inputStream.Length)
+                         {
+                             var buffer = new byte[1024*1024];
+                             var bytesRead = inputStream.Read(buffer, 0, buffer.Length);
+                             if (bytesRead < buffer.Length)
+                             {
+                                 var newBuffer = new byte[bytesRead];
+                                     for (int i = 0; i < bytesRead; i++)
+                                     {
+                                         newBuffer[i] = buffer[i];
+                                     }
+                                 buffer = newBuffer;
+                              }       
+                              outputStream.Write(buffer, 0, buffer.Length);
+                          }
+                    }
+                }  
+                }
+            }
         }
     }
 }
-
